4ee6c76dd19e29a2254ea11b8a07e14d2cf945c8
shashidhar-gr <shashidhargr26@gmail.com>
2019-10-06 12:05:50 +0530

    Completed product of array elements

--- Arrays/Challenge_4.spec.js
+++ Arrays/Challenge_4.spec.js
@@ -5,7 +5,14 @@ describe('findProduct', function() {
     it('should return empty array since array is empty.', function() {
         assert.deepEqual(findProduct([]), [])
     })
-    it('should return proper array since array has elements.', function() {
+    it('should return proper array since array has all positive elements.', function() {
         assert.deepEqual(findProduct([1,2,3,4]), [24,12,8,6])
     })
+    it('should return proper array since array has mix of negative and positive elements.', function() {
+        assert.deepEqual(findProduct([-1,2,3,4]), [24,-12,-8,-6])
+    })
+    it('should return proper array since array has all negative elements.', function() {
+        assert.deepEqual(findProduct([-1,-2,-3,-4]), [-24,-12,-8,-6])
+    })
+    
 })
